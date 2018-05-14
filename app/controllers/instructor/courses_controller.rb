5f6bdeb32947e71f432822f898e617b3480fc965
John Henderson <johnhenderson@fastmail.fm>
2018-05-14 18:10:01 +0000

    Lessons only viewable by users who have enrolled

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -20,15 +20,16 @@ class Instructor::CoursesController < ApplicationController
 
   private
 
-  def require_authorized_for_current_course
-    if current_course.user != current_user
+  def require_authorized_for_current_course # Define this method
+    if current_course.user != current_user # If current_user isn't on the current_course.user list
       render plain: "Unauthorized", status: :unauthorized
     end
   end
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:id])
+    @current_course ||= Course.find(params[:id]) # Copy all info from Course table for this course
+    # (ID'd in the URL) into the current_course instance variable
   end
 
   def course_params
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+      # render plain: "Unauthorized", status: :unauthorized
+      redirect_to course_path(current_lesson.section.course), alert: 'You must enroll before you can view this lesson'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
